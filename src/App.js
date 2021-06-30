42dd98014c6f6116166fafa1661a80a1e43a6427
natarafael <natan_rafa13@icloud.com>
2021-06-30 13:36:33 -0300

    new file:   public/WICKED.png

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import { useEffect } from "react";
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
+import { WiDayCloudy } from "react-icons/wi";
 
 import WICKED from "./WICKED.png";
 
@@ -13,24 +14,30 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="page-container">
-      <div className="content-wrap">
-        <header>
-          <Link to="/">
-            <img src={WICKED} alt="logo" className="logo" />
-          </Link>
-          WICKED STOCKS
-        </header>
-        <NavBar />
-        <Switch>
-          <Route path="/" exact>
-            <Chart />
-          </Route>
-          <Route path="/cryptoMarket">
-            <CryptoMarket />
-          </Route>
-        </Switch>
-      </div>
+    <div>
+      <header className="heads">
+        <Link to="/">
+          <img src={WICKED} alt="logo" className="logo" />
+        </Link>
+        <div className="title">WICKED STOCKS</div>
+        <div className="weather">
+          <a href="http://localhost:3001" target="_blank" rel="noreferrer">
+            <WiDayCloudy />
+          </a>
+        </div>
+      </header>
+
+      <NavBar />
+
+      <Switch>
+        <Route path="/" exact>
+          <Chart />
+        </Route>
+        <Route path="/cryptoMarket">
+          <CryptoMarket />
+        </Route>
+      </Switch>
+
       <Footer />
     </div>
   );
